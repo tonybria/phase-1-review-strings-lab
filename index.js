d0d859b744cf68cbc2b6f9c875c9e6315b6177f9
tony-brian <btoni3823@gmail.com>
2023-07-02 18:41:41 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your code in this file!
+let currentUser = "Grace Hopper";
+
+let WelcomingMessage = "Welcome to Flatbook, ";
+let userName = prompt("Tony:");
+let WelcomeMessage = `Welcome to Flatbook, ${userName}`;
+
+console.log(welcomeMessage); 
+
+
+let welcomeMessage = "Welcome to Flatbook, ";
+let UserName = prompt("Tony:");
+welcomeMessage += userName + "!";
+
+console.log(welcomeMessage);
+
+
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`;
+
+let shortGreeting = "Welcome, " + currentUser.charAt(0) + "!";
+
+
+
+
+
+
+  
